9ff1aa4963de2cd370c76fbc17cbbf18203aa07d
m2in <35535283+m2in@users.noreply.github.com>
2018-02-10 11:31:17 +0300

    Delete start_.bat

--- lesson4/start_.bat
+++
@@ -1,8 +0,0 @@
-MD css, js, img, sass, fonts
-
-@echo Start file>css\style.css
-@echo Start file>js\main.js
-@echo Start file>sass\style.sass
-@echo Start file>index.html
-
-browser-sync start -s -f "*.html, css/*.css, js/*.js, sass/*.sass"
